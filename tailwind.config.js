32bddbb2f070387f022325106040659a18075973
raflimardhian <raflimardhian13@gamil.com>
2023-07-21 23:26:10 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'background': "url('./images/Background.svg')"
+      }
+    },
+  },
+  plugins: [],
+}
+
